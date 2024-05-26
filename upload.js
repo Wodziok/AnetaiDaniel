bdcfd3250c77211ff83b1ed6de097c14225e7d75
Wodziok <woda.daniel@gmail.com>
2024-05-26 06:16:10 +0100

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,49 @@
+let CLIENT_ID = 964538086586-1s6s1l2uesvvq5hp8k4pub0oksel765t.apps.googleusercontent.com
+
+let API_KEY = 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
+let DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+let SCOPES = 'https://www.googleapis.com/auth/drive.file';
+
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        gapi.auth2.getAuthInstance().signIn();
+    });
+}
+
+function uploadFiles() {
+    let files = document.getElementById('fileInput').files;
+    if (files.length > 0) {
+        for (let i = 0; i < files.length; i++) {
+            let file = files[i];
+            let metadata = {
+                'name': file.name,
+                'mimeType': file.type
+            };
+            let accessToken = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
+            let form = new FormData();
+            form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
+            form.append('file', file);
+
+            fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart', {
+                method: 'POST',
+                headers: new Headers({ 'Authorization': 'Bearer ' + accessToken }),
+                body: form
+            }).then((response) => {
+                return response.json();
+            }).then((data) => {
+                console.log(data);
+            });
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', handleClientLoad);
